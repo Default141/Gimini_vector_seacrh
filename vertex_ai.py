aa210906a724ee05457ab36b3cd2dd519f8e6879
Jittiwat26 <reconx26@gmail.com>
2024-09-26 15:19:52 +0700

    Dev_RAG_LOTTERY
    Code BOT RAG ORDER

--- vertex_ai.py
+++ vertex_ai.py
@@ -13,12 +13,12 @@ from langchain_core.chat_history import BaseChatMessageHistory
 # Project and environment settings
 PROJECT_ID = "solutions-data"
 DATASET = "companyData"
-TABLEEMBED = "Fortune_Telling"
+TABLEEMBED = "Order_PDF_ALL"
 REGION = "asia-southeast1"
 
-JSON_KEY_PATH = "credential/vertexAi.json"
+# JSON_KEY_PATH = "credential/vertexAi.json"
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = JSON_KEY_PATH
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = JSON_KEY_PATH
 
 
 embedding_model = VertexAIEmbeddings(
@@ -32,34 +32,39 @@ bq_vector_datasource = BigQueryVectorSearch(
     embedding=embedding_model,
     content_field="text",
     text_embedding_field="embedding"
-
 )
 
 # Function to configure AI settings
-def ai_config(model_name="gemini-1.5-pro-001", max_tokens=512, max_retries=100):
+def ai_config(model_name="gemini-1.5-pro-001", max_tokens=512, max_retries=6):
     llm = ChatVertexAI(model_name=model_name, max_tokens=max_tokens, max_retries=max_retries)
 
+
     config_prompt = """
-      You are หมูตู, a friendly AI assistant. 😊
-    Context: {context}
-    Input: {input}
-    You have previous context with the user. Use this information to provide an accurate response.
+You are จิดริ้ด, a friendly AI assistant. 😊
+Context: {context}
+Input: {input}
+You have previous context with the user. Use this information to provide an accurate response.
 
-    User Name: {user_name}
-    No need to greet the user again.
+User Name: {user_name}
+No need to greet the user again.
 
-    **Instructions:**
-    -ถ้ามีการถามเกี่ยวกับเรื่องความฝัน ทำนายฝัน ให้ดึงข้อมูลมาจาก {context} เพื่อตอบคำถาม user
-        Example: User says: ฉันฝันเห็นงู
-        Output: ถ้าฝันเห็นงูนั้นหมายถีง คุณอาจจะเจอเนื้่อคู่ เลขนำโชคคือ 00, 31
-    -ถ้าข้อมมูลนั้นไม่ตรงกับ ข้อมูลใน {context} ให้ตัว หมูตู ทำนายฝันเองและ ให้เลขนำโชคเอง
-    -โดยให้แสดงความคิดเห็นไปในเชิงบวกเสมอ
-    **Refer to the user by their name throughout the conversation.**
+**Instructions:**
+1. Only use the provided information to respond. Do not invent or guess data beyond what is available.
+2. Respond naturally and concisely as if you are a real person helping with data and lottery-related questions.
+3. ถ้าถามเกี่ยวกับ Order ให้ตอบ เลข Order ที่มีของลูกค้าทั้งหมด และจำนวนลอตเตอรี่ที่มี ในแต่ล่ะ Orderด้วย
+4.If it is about the total price of that customer name, look in the 'totalPrice'.
+5.If there is a request for customer or user information, please reply about ชื่อ-นามสกุลลูกค้า,รหัสสมาชิก,UID,เบอร์โทร
+6. Answer in Thai.
+7. ถ้ามีการถามเกี่ยวกับ ยอดขายทั้งหมดเท่าไหร่ ให้บวกเลขจาก ยอดราคาสุทธิ แล้วนำมาตอบ
+8. ถ้ามีการถามเกี่ยวกับ จำนวนลอตเตอรี่ที่ขายได้ทั้งหมดเท่าไหร่ กี่ใบ ให้บวกเลขจาก จำนวนยอดขายสุทธิ แล้วนำมาตอบ
+9. ถ้ามีการถามว่าลูกค้าคนไหน ซื้อเยอะที่สุด ในดูจาก Order ของแต่ล่ะคน และรวม จำนวนยอดขายสุทธิ ของคนนั้นเข้าด้วยกันเพื่อเปรียบเทียบว่าใครซื้อเยอะสุด
+
+**Refer to the user by their name throughout the conversation.**
 
     """
 
     return config_prompt, llm
-#
+
 # In-memory store for session history
 store = {}
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
@@ -68,7 +73,7 @@ def get_session_history(session_id: str) -> BaseChatMessageHistory:
     return store[session_id]
 
 # Function to batch retrieve results using fetch_k
-def batch_retrieval_with_fetch_k(retriever, query, batch_size=100, fetch_k=200, num_batches=100):
+def batch_retrieval_with_fetch_k(retriever, query, batch_size=100, fetch_k=200, num_batches=10):
     results = []
     for _ in range(num_batches):
         batch_results = retriever.get_relevant_documents(query, k=batch_size, fetch_k=fetch_k)
@@ -114,6 +119,7 @@ def prompt_ai(query, model_name="gemini-1.5-pro-001", session_id="", user_name="
     history_aware_retriever = create_history_aware_retriever(llm, retriever, prompt)
     question_answer_chain = create_stuff_documents_chain(llm, prompt)
     chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
     # try:
     conversational_rag_chain = RunnableWithMessageHistory(
         chain,
@@ -128,16 +134,12 @@ def prompt_ai(query, model_name="gemini-1.5-pro-001", session_id="", user_name="
         # {"input": query, "context": context, "user_name": user_name, "documents": documents},
         config={"configurable": {"session_id": session_id}},
     )
-
-
     answer = result.get("answer", "No answer found.")
     # except Exception as e:
     #     print(f"Error during invocation: {e}")
     #     answer = "An error occurred."
 
     # Debug result
-    # print(f"Result: {answer}")
-#
-    return answer
-#
+    print(f"Result: {answer}")
 
+    return answer
